6deb87358bdcfd1ae95fd0bcd19a38d8b50dd5ef
JersonUCB <jerson.garcia@ucb.edu.bo>
2023-04-11 13:23:40 -0400

    Pruebas Unitarias Alfil

--- Tests/Piezas.Tests/alfil.test.js
+++ Tests/Piezas.Tests/alfil.test.js
@@ -1,3 +1,22 @@
-test("Sanity check", () => {
-    expect(true).toBe(true);
+const alfil = require("../../Piezas/alfil")
+global.matrizTablero = require("../../Tablero/matrizTablero")
+global.recorrerDiagonal = require("../../Funciones/funciones").recorrerDiagonal
+
+test("Primera prueba para validar el movimiento del alfil.", () => {
+    let x1 = 0, y1 = 0, x2 = 1, y2 = 0;
+    matrizTablero[x1][x1].color = "negro"
+    matrizTablero[x2][y2].color = "negro"
+    expect(alfil (x1,y1,x2,y2)).toBe(false);//Falso pq deberia avanzar diagonal no vertical
+});
+test("Segunda prueba para validar el movimiento del alfil.", () => {
+    let x1 = 1, y1 = 1, x2 = 0, y2 = 0;
+    matrizTablero[x1][x1].color = "blancas"
+    matrizTablero[x2][y2].color = "blancas"
+    expect(alfil (x1,y1,x2,y2)).toBe(true);
+});
+test("Tercera prueba para validar el movimiento del alfil.", () => {
+    let x1 = 1, y1 = 1, x2 = 0, y2 = 0;
+    matrizTablero[x1][x1].color = "blancas"
+    matrizTablero[x2][y2].color = "blancas"
+    expect(alfil (x1,y1,x2,y2)).toBe(false);
 });
