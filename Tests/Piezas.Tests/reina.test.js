70dd86ede8b6cc1866e28efb101a05b289461352
Ricardo <jesus.pelaez@ucb.edu.bo>
2023-04-14 11:05:07 -0600

    Agregar descripcion y cuarta version unit tests para la reina.

--- Tests/Piezas.Tests/reina.test.js
+++ Tests/Piezas.Tests/reina.test.js
@@ -3,30 +3,30 @@ global.matrizTablero = require("../../Tablero/matrizTablero")
 global.recorrer = require("../../Funciones/funciones").recorrer
 global.recorrerDiagonal = require("../../Funciones/funciones").recorrerDiagonal
 
-test("Primera prueba para validar el movimiento de la reina.", () => {
+test("Primera prueba para validar el movimiento de la reina negro de la posicion [0-0] al [0-1], destino denegado por que se encuentra una ficha aliada.", () => {
     let x1 = 0, y1 = 0, x2 = 1, y2 = 0;
     matrizTablero[x1][y1].color = "negro";
     matrizTablero[x2][y2].color = "negro";
-    expect(reina(x1,y1,x2,y2)).toBe(false);
+    expect(reina(x1,y1,x2,y2)).toBeFalsy();
 });
 
-test("Segunda prueba para validar el movimiento de la reina.", () => {
+test("Segunda prueba para validar el movimiento de la reina negro de la posicion [1-0] al [0-0], destino aceptado con la opcion de comer una ficha rival.", () => {
     let x1 = 0, y1 = 1, x2 = 0, y2 = 0;
     matrizTablero[x1][y1].color = "negro";
     matrizTablero[x2][y2].color = "blanca";
-    expect(reina(x1,y1,x2,y2)).toBe(true);
+    expect(reina(x1,y1,x2,y2)).toBeTruthy();
 });
 
-test("Tercera prueba para validar el movimiento de la reina.", () => {
+test("Tercera prueba para validar el movimiento de la reina negro de la posicion [1-1] al [0-0], destino aceptado con la opcion de comer una ficha rival.", () => {
     let x1 = 1, y1 = 1, x2 = 0, y2 = 0;
     matrizTablero[x1][y1].color = "negro";
     matrizTablero[x2][y2].color = "blanca";
-    expect(reina(x1,y1,x2,y2)).toBe(true);
+    expect(reina(x1,y1,x2,y2)).toBeTruthy();
 });
 
-test("Cuarta prueba para validar el movimiento de la reina.", () => {
+test("Cuarta prueba para validar el movimiento de la reina negro de la posicion [0-0] al [1-3], destino denegado por que el movimiento es invalido.", () => {
     let x1 = 0, y1 = 0, x2 = 3, y2 = 1;
     matrizTablero[x1][y1].color = "negro";
     matrizTablero[x2][y2].color = "blanca";
-    expect(reina(x1,y1,x2,y2)).toBe(false);
+    expect(reina(x1,y1,x2,y2)).toBeFalsy();
 });
